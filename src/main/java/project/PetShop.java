3218d4b0e6dca83ce8d3df0926fd8e44173b557d
Helene Selvig <helenkse@stud.ntnu.no>
2024-03-31 01:26:59 +0100

    tester ut scenebuilder

---
+++ src/main/java/project/PetShop.java
@@ -0,0 +1,13 @@
+package project;
+
+public class PetShop {
+    // private (noe) animal; ?? lagre valget av dyr (det dyret man har trykket på) evt lage en Animal-klasse??
+
+    public PetShop() {
+        //evt validering om man tar inn noe i konstruktøren
+        //this.animal = animal;
+    }
+
+    //logikk for hva som skjer når man har valgt et dyr?
+
+}
