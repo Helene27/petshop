83a74fd9bc1511fd6fd5fb873da010d742908268
Mari Gjul <marigjul99@gmail.com>
2024-04-04 16:08:30 +0200

    La til if statement: sjekke felt

--- src/main/java/project/SecondaryController.java
+++ src/main/java/project/SecondaryController.java
@@ -2,16 +2,29 @@ package project;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class SecondaryController {
     @FXML
     TextField nameField;
 
+    @FXML 
+    Label outputLabel;
+
+    @FXML
+    Button enterButton;
+
     @FXML
     private void switchToTertiary() throws IOException {
         String name = nameField.getText();
-        System.out.println(name);
-        App.setRoot("tertiary");
+
+        if (name.isEmpty()) {
+            this.outputLabel.setText("Give your pet a name!");
+        } else {
+            System.out.println(name);
+            App.setRoot("tertiary");
+        }
     }
 }
