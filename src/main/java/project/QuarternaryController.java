b70a487fdf142c880a900628f68e3735eeed3f7b
Helene Selvig <helenkse@stud.ntnu.no>
2024-04-09 00:20:35 +0200

    endringer i stil og bilder

--- src/main/java/project/QuarternaryController.java
+++ src/main/java/project/QuarternaryController.java
@@ -1,9 +1,17 @@
 package project;
 
 import java.io.IOException;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class QuarternaryController {
+
+    @FXML
+    public void closeApplication(ActionEvent event) throws IOException {
+        Platform.exit();
+    }
 
     @FXML
     private void switchToPrimary() throws IOException {
