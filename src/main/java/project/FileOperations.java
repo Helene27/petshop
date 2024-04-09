f9e5f24937a6eea075b112e6004314141d98f8d6
Helene Selvig <helenkse@stud.ntnu.no>
2024-04-09 13:59:16 +0200

    fil lese og skrive

--- src/main/java/project/FileOperations.java
+++ src/main/java/project/FileOperations.java
@@ -1,31 +1,43 @@
 package project;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 
 public class FileOperations {
     
-    private String text;
-
+    private String textIn;
+    private String textOut;
+    
+    
     public FileOperations(String text) {
-        this.text = text;
+        this.textIn = text;
+        this.textOut = "";
     }
-
-
+    
+    
     public void writeToFile(String filename) {
         try {
             PrintWriter writer = new PrintWriter(filename);
-            writer.println(text);
+            writer.println(textIn);
             writer.flush();
             writer.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
-
-    public void readFromFile(String filename) {
+    
+    public String readFromFile() throws FileNotFoundException{
+        Scanner scanner = new Scanner(new File("src/main/resources/project/Animals.txt"));
         
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            textOut += line;
+        }
+        scanner.close();
+        return textOut;
     }
 
 }
--- src/main/java/project/QuarternaryController.java
+++ src/main/java/project/QuarternaryController.java
@@ -1,18 +1,35 @@
 package project;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class QuarternaryController {
 
+    FileOperations operation;
+
+    @FXML
+    Label certificateText;
+
+    String text;
+
+    @FXML
+    public void initialize() throws FileNotFoundException{
+        operation = new FileOperations("lol");
+        text = operation.readFromFile();
+        certificateText.setText(text);
+
+    }
+    
     @FXML
     public void closeApplication(ActionEvent event) throws IOException {
         Platform.exit();
     }
-
+    
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
