57983052057d107d76115784eeea97a9ebc2bab7
Mari Gjul <marigjul99@gmail.com>
2024-04-11 09:17:46 +0200

    lagde to tester

---
+++ src/main/java/test/DogTest.java
@@ -0,0 +1,40 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import project.Dog;
+
+public class DogTest {
+    private Dog dog;
+    
+    
+    @BeforeEach
+    public void setup() {
+        dog = new Dog();
+    }
+    
+    //Test 1
+    //sjekker om den kalkulerer riktig
+    @Test
+    public void testCalculateAge() {
+        int humanAge = 8;
+        int expectedDogAge = humanAge * 7;
+        assertEquals(expectedDogAge, dog.calculateAge(humanAge));
+    }
+
+    //Test 2
+    //tester om metoden thrower exception om human age er < 0
+    @Test
+    public void testCalculateAgeWithNegativeAge(){
+        assertThrows(IllegalAccessException.class, () -> {
+            dog.calculateAge(-20);
+        });
+    }
+
+
+
+}
