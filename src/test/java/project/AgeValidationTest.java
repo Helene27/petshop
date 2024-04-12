0cf28688844672093d0bb697b57761ef68e19f6d
Helene Selvig <helenkse@stud.ntnu.no>
2024-04-12 14:57:38 +0200

    SISTE

--- src/test/java/project/AgeValidationTest.java
+++ src/test/java/project/AgeValidationTest.java
@@ -3,9 +3,6 @@ package project;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.io.File;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
